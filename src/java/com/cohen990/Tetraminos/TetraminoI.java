44c2c3f0bb025c502fa4a77d91125cdd95202238
Dan Cohen <dan.cohen@euromoneyplc.com>
2017-11-05 15:59:02 +0000

    refactor tetraminos

---
+++ src/java/com/cohen990/Tetraminos/TetraminoI.java
@@ -0,0 +1,14 @@
+package com.cohen990.Tetraminos;
+
+import java.awt.*;
+
+public class TetraminoI extends Tetramino{
+    public TetraminoI(){
+        Points = new Point[][] {
+            { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1) },
+            { new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3) },
+            { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1) },
+            { new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3) }
+        };
+    }
+}
---
+++ src/java/com/cohen990/Tetraminos/TetraminoO.java
@@ -0,0 +1,14 @@
+package com.cohen990.Tetraminos;
+
+import java.awt.*;
+
+public class TetraminoO extends Tetramino{
+    public TetraminoO(){
+        Points = new Point[][] {
+            { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
+            { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
+            { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
+            { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) }
+        };
+    }
+}
